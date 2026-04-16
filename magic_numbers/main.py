f31921cc5e9d05763aab4eec092312192ac2a908
Ati <124589850+coca-cola-fanta@users.noreply.github.com>
2026-04-17 00:30:57 +0200

    checks if it's a palindrome or not and started other basic functions

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,9 +1,39 @@
 from pathlib import Path
 
 
+def is_palindrome(x):
+    return x == x[::-1]
+
+def add_one(x):
+    return str(int(x) + 1)
+
+def handle_odd(x):
+    n = len(x)
+    left = x[:n // 2]
+    mid = x[n // 2]
+    right = x[n // 2 + 1:]
+
+def handle_even(x):
+    pass
+
+def solve(x):
+    if is_palindrome(x): x = add_one(x)
+
+    if len(x) % 2 != 0:
+        handle_odd(x)
+    else:
+        handle_even(x)
+
+
+
+
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    for line in data.splitlines():
+        line = line.strip()
+        if line:
+            result = is_palindrome(line)
+            print(f"A {line} palindrom-e {result}")
 
 
 if __name__ == "__main__":
